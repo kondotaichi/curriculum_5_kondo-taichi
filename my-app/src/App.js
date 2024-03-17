f0ee0075d4734d4d018d3e0c4967508a73b05076
kondotaichi <ubjdij210@gmail.com>
2024-03-17 11:20:48 +0900

    課題2出し直し

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
+{count}
+<button onClick={() => setCount(count + 1)}>Button</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
