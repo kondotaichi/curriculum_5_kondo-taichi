3b4931fb989ac86c949097a3e833f456fe65a795
kondotaichi <ubjdij210@gmail.com>
2024-06-15 12:37:30 +0900

    認証

---
+++ authentication/my-app/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
---
+++ authentication/my-app/src/components/LoginForm.tsx
@@ -0,0 +1,69 @@
+// src/components/LoginForm.tsx
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { fireAuth } from "../firebase/config";
+
+export const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signInWithEmailPassword = (): void => {
+    signInWithEmailAndPassword(fireAuth, email, password)
+      .then(res => {
+        const user = res.user;
+        alert("ログインユーザー: " + user.email);
+      })
+      .catch(err => {
+        const errorMessage = err.message;
+        alert(errorMessage);
+      });
+  };
+
+  const signUpWithEmailPassword = (): void => {
+    createUserWithEmailAndPassword(fireAuth, email, password)
+      .then(res => {
+        const user = res.user;
+        alert("新規ユーザー作成: " + user.email);
+      })
+      .catch(err => {
+        const errorMessage = err.message;
+        alert(errorMessage);
+      });
+  };
+
+  const signOutWithEmailPassword = (): void => {
+    signOut(fireAuth).then(() => {
+      alert("ログアウトしました");
+    }).catch(err => {
+      alert(err);
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <input 
+          type="email" 
+          placeholder="メールアドレス" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <input 
+          type="password" 
+          placeholder="パスワード" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+      </div>
+      <button onClick={signInWithEmailPassword}>
+        ログイン
+      </button>
+      <button onClick={signUpWithEmailPassword}>
+        新規ユーザー作成
+      </button>
+      <button onClick={signOutWithEmailPassword}>
+        ログアウト
+      </button>
+    </div>
+  );
+};
